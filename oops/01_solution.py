855a9326656a353fb0c9e805fbecdb0aa95b4977
Prashant Kumar <prashantkumar6425@gmail.com>
2025-07-23 20:43:54 +0530

    update oops notes

--- oops/01_solution.py
+++ oops/01_solution.py
@@ -1,7 +1,15 @@
 class Car:
+    total_car = 0
+
+    @staticmethod 
+    def general_description():
+        return "Cars are the 4th largest pollution producing factor!"
+
+
     def __init__(self, brand, model):   # Constructor ==> __init__
+        Car.total_car += 1
         self.__brand = brand    # ==> __anything shows private name
-        self.model = model
+        self.__model = model
     
     '''Encapsulation'''
     # setter method
@@ -9,7 +17,7 @@ class Car:
         self.__brand = brand
 
 
-    def get_details(self):
+    def get_brand(self):
         return self.__brand + " !"
 
     '''polymorphism'''
@@ -17,7 +25,15 @@ class Car:
         return "Petrol or Deisel"
     
     def full_Display(self):
-        return f"{self.__brand}: {self.model}"
+        return f"{self.__brand}: {self.__model}"
+
+    '''Property decorator'''
+    @property
+    def model(self):
+        return self.__model
+    
+
+
 
 ''' Inhertence '''
 class ElectricCar(Car):
@@ -27,7 +43,7 @@ class ElectricCar(Car):
 
     '''polymorphism''' # ==> method overriding
     def fuel_type(self):
-        return "Petrol or Deisel"
+        return "Electricity"
 
 
 my_2nd_car = ElectricCar("Tesla", "Model S","battery")
@@ -35,15 +51,48 @@ my_2nd_car_desc = my_2nd_car.full_Display()
 print(my_2nd_car_desc)
 print("Fuel Type: ", my_2nd_car.fuel_type())
 
+print("-------------------")
+
 my_car = Car("Toyota", "Corola")
 # print(my_car.__brand)
-print(my_car.get_details())
-my_car.set_brand("Prashant")
-print(my_car.get_details())
-print(my_car.model)
+# print(my_car.get_brand())
+# my_car.set_brand("Prashant")
+# print(my_car.get_brand())
+print(my_car.full_Display())
 print("Fuel Type: ", my_car.fuel_type())
+print(my_car.model)
 
-# my_new_car = Car("Tata","safari")
-# print(my_new_car.model)
-# print(my_2nd_car.get_details())
+print("-------------------")
+my_new_car = Car("Tata","safari")
+print(my_new_car.model)
+print(my_2nd_car.get_brand())
 
+print("-------------------")
+print(Car.total_car)
+
+# # isinstance(obj, classinfo) ==> check the is object is an instance of that class which we compare (gives: true/false)
+# if isinstance(my_2nd_car, Car):
+#     print("Tesla Car is an instance of Car class")
+# if isinstance(my_2nd_car, ElectricCar):
+#     print("Tesla Car is an instance of Electric class")
+
+
+
+''' Multiple Inheritance '''
+class Battery:
+    def battery_details(self):
+        return "This is 4-octa multi-cellular battery."
+
+class Engine:
+    def engine_details(self):
+        return "This is high performance and long lasting engine."
+    
+
+class ElectricCarTwo(Battery, Engine, Car):
+    pass
+
+
+new_electric_car = ElectricCarTwo("Tesla", "Model S")
+
+print(new_electric_car.battery_details())
+print(new_electric_car.engine_details())
--- oops/getter_setter.py
+++ oops/getter_setter.py
@@ -1,33 +1,41 @@
 class Student:
+    total_student = 0
+
+    @staticmethod
+    def greeting():
+        return "Hello Bachooo!"
+
     def __init__(self,age = 0, name = "Unknown"):
-        self._age = age
-        self._name = name
+        Student.total_student += 1
+        self.__age = age
+        self.__name = name
     
     # Eligant manner to intialize the getter and setter
     @property
     def age(self):
         # print("getter method called.")
-        return self._age
+        return self.__age
 
     @age.setter
     def age(self, a):
         if(a < 5):
             raise ValueError("You are not eligible for school! Go for play and keep learing and exploring! :)")
         # print("setter method called")
-        self._age = a
+        self.__age = a
     
     # Alternative method
-    def get_name(self):
-        return self._name
+    def get__name(self):
+        return self.__name
 
-    def set_name(self, n):
-        self._name = n
+    def set__name(self, n):
+        self.__name = n
 
     def get_details(self):
-        return f"{self._name}(age:{self._age}): Welcome to guru-kul!"
+        return f"{self.__name}(age:{self.__age}): Welcome to guru-kul!"
 
     
 new_student = Student()
 new_student.age = 22
-new_student.set_name("Prashant")
+new_student.set__name("Prashant")
 print(new_student.get_details())
+print(Student.total_student)
